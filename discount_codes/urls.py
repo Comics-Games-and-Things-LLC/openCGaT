c7baccb22689f3d0149a58df49836e8951d688e3
Noah Stephen Haskell <nstephenh@gmail.com>
2023-12-10 18:35:23 -0600

    Breakdown referral by discount code, improve speed with aggregate, report number of code usages

--- discount_codes/urls.py
+++ discount_codes/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('referrers/', views.referral_index, name='referrals_index'),
     path('referrers/<referrer_slug>', views.referral_report, name='referrer_report'),
+    path('referrers/<referrer_slug>/<code>', views.referral_code_report, name='referrer_code_report'),
 
 ]
--- discount_codes/views.py
+++ discount_codes/views.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+
+from django.db.models import Sum, F
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
@@ -64,28 +67,63 @@ def referral_report(request, partner_slug, referrer_slug):
     codes = DiscountCode.objects.filter(referrer=referrer, partner_discounts__partner=partner)
 
     kickback_earnings = Money(0, "USD")
+    sales_total = Money(0, "USD")
 
-    kickback_lines = {}  # Cart: {earnings: Money, items: str}
+    code_details = {}
     for code in codes:
-        kickback_percentage = code.partner_discounts.get(partner=partner).referrer_kickback
-        referred_lines = CheckoutLine.objects.exclude(cart__status=Cart.CANCELLED, cancelled=True).filter(
+        code_details[code] = {}
+
+        kickback_percentage = Decimal(code.partner_discounts.get(partner=partner).referrer_kickback / 100)
+        sales_amount = CheckoutLine.objects.exclude(cart__status=Cart.CANCELLED, cancelled=True).filter(
             cart__status__in=[Cart.SUBMITTED, Cart.PAID, Cart.COMPLETED], cart__discount_code=code).order_by(
-            "cart__date_paid")
-        for line in referred_lines:
-            print(line)
-            earnings_this_line = line.price_per_unit_at_submit * line.quantity * (kickback_percentage / 100)
-            kickback_earnings += earnings_this_line
-            if line.cart not in kickback_lines:
-                kickback_lines[line.cart] = {"kickback": earnings_this_line, "items": [line.item.product.name]}
-            else:
-                kickback_lines[line.cart]["kickback"] += earnings_this_line
-                kickback_lines[line.cart]["items"].append(line.item.product.name)
+            "cart__date_paid").aggregate(sum=Sum(F('price_per_unit_at_submit') * F('quantity')))["sum"]
+
+        if sales_amount:
+            kickback_amount = Money(sales_amount * kickback_percentage, "USD")
+            kickback_earnings += kickback_amount
+            code_details[code]["kickback"] = kickback_amount
+            sales_total += Money(sales_amount, "USD")
+            code_details[code]["sales"] = sales_total
+
+            code_details[code]["cart_count"] = Cart.submitted.filter(discount_code=code).count()
+            code_details[code]["click_count"] = CodeUsage.objects.filter(code=code).count()
 
     context = {"partner": partner,
                "referrer": referrer,
                "kickback_earnings": kickback_earnings,
                "codes": codes,
-               "kickback_lines": kickback_lines,
+               "code_details": code_details,
                }
 
     return render(request, "discount_codes/referrer_summary.html", context)
+
+
+def referral_code_report(request, partner_slug, referrer_slug, code):
+    partner = get_partner_or_401(request, partner_slug)
+    referrer = get_object_or_404(Referrer, slug=referrer_slug)
+    code = get_object_or_404(DiscountCode, referrer=referrer, partner_discounts__partner=partner, code=code)
+
+    kickback_earnings = Money(0, "USD")
+
+    kickback_lines = {}  # Cart: {earnings: Money, items: str}
+    kickback_percentage = Decimal(code.partner_discounts.get(partner=partner).referrer_kickback / 100)
+    referred_lines = CheckoutLine.objects.exclude(cart__status=Cart.CANCELLED, cancelled=True).filter(
+        cart__status__in=[Cart.SUBMITTED, Cart.PAID, Cart.COMPLETED], cart__discount_code=code).order_by(
+        "cart__date_paid")
+    for line in referred_lines:
+        print(line)
+        earnings_this_line = line.price_per_unit_at_submit * line.quantity * kickback_percentage
+        kickback_earnings += earnings_this_line
+        if line.cart not in kickback_lines:
+            kickback_lines[line.cart] = {"kickback": earnings_this_line, "items": [line.item.product.name]}
+        else:
+            kickback_lines[line.cart]["kickback"] += earnings_this_line
+            kickback_lines[line.cart]["items"].append(line.item.product.name)
+    context = {"partner": partner,
+               "referrer": referrer,
+               "code": code,
+               "kickback_earnings": kickback_earnings,
+               "kickback_lines": kickback_lines,
+               }
+
+    return render(request, "discount_codes/referrer_code_report.html", context)
