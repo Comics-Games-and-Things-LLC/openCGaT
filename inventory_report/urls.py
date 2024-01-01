73c4627e27d5cc2f524ad933227e86d73f6f363b
Noah Stephen Haskell <nstephenh@gmail.com>
2024-01-01 15:21:13 -0600

    Fix delete from location on inv_report

--- inventory_report/urls.py
+++ inventory_report/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('<report_id>/<location_id>/', views.report_details, name='report'),
     path('<report_id>/<location_id>/add/<barcode>/', views.add, name='report_add'),
     path('<report_id>/delete/<report_line_id>/<confirm>', views.delete_inv_report_line, name='delete_inv_report_line'),
-    path('<report_id>/<location_id>/delete/<report_line_id>', views.delete_inv_report_line,
-         name='delete_inv_report_line'),
     path('<report_id>/<location_id>/delete/<report_line_id>/<confirm>', views.delete_inv_report_line,
          name='delete_inv_report_line'),
 
