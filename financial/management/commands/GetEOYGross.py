75d549d624556edd59c8a4f3d5af079fdc394d3a
Noah Stephen Haskell <nstephenh@gmail.com>
2024-03-03 17:58:44 -0600

    write gross data to CSV

--- financial/management/commands/GetEOYGross.py
+++ financial/management/commands/GetEOYGross.py
@@ -1,3 +1,4 @@
+import csv
 from decimal import Decimal
 
 import moneyed
@@ -22,25 +23,37 @@ class Command(BaseCommand):
         tax = Money(0, 'USD')
         total = Money(0, 'USD')
         total_collected = Money(0, 'USD')
-        for cart in tqdm(Cart.submitted.filter(status__in=[Cart.PAID, Cart.COMPLETED], date_paid__year=year)
-                                 .order_by("date_paid")):
-            gross += cart.get_total_subtotal()
-            gross += cart.final_ship
-            shipping += cart.final_ship
-            tax += cart.final_tax
-            total += cart.final_total
-            valhalla_subtotal += cart.get_subtotal_after_cancellations()
-            try:
-                if cart.total_paid:
-                    if cart.total_paid.amount > (cart.final_total.amount + Decimal(.01)):
-                        log(f, f"{cart} was overpaid, we likely gave change")
-                    if cart.total_paid.amount < (cart.final_total.amount - Decimal(.01)):
-                        log(f, f"{cart} was underpaid!")
+        with open('reports/eoy_gross_{}.csv'.format(year), 'w',
+                  newline='') as csvfile:
+            fieldnames = ['Cart', 'subtotal', 'final_total', 'subtotal_after_cancellations', 'collected']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for cart in tqdm(Cart.submitted.filter(status__in=[Cart.PAID, Cart.COMPLETED], date_paid__year=year)
+                                     .order_by("date_paid")):
+                row_data = {"Cart": cart.id,
+                            'subtotal': cart.get_total_subtotal(),
+                            'final_total': cart.final_total,
+                            'subtotal_after_cancellations': cart.get_subtotal_after_cancellations(),
+                            }
+                gross += cart.get_total_subtotal()
+                gross += cart.final_ship
+                shipping += cart.final_ship
+                tax += cart.final_tax
+                total += cart.final_total
+                valhalla_subtotal += cart.get_subtotal_after_cancellations()
+                try:
+                    if cart.total_paid:
+                        if cart.total_paid.amount > (cart.final_total.amount + Decimal(.01)):
+                            log(f, f"{cart} was overpaid, we likely gave change")
+                        if cart.total_paid.amount < (cart.final_total.amount - Decimal(.01)):
+                            log(f, f"{cart} was underpaid!")
 
-                # Use the minimum of final total and total paid,
-                total_collected += min(cart.final_total, cart.total_paid or Money(0, 'USD'))
-            except moneyed.classes.CurrencyDoesNotExist:
-                pass  # If total_paid is not set, currency will be XYZ, which crashes moneyed
+                    # Use the minimum of final total and total paid,
+                    total_collected += min(cart.final_total, cart.total_paid or Money(0, 'USD'))
+                    row_data['collected'] = min(cart.final_total, cart.total_paid or Money(0, 'USD'))
+                except moneyed.classes.CurrencyDoesNotExist:
+                    pass  # If total_paid is not set, currency will be XYZ, which crashes moneyed
+                writer.writerow(row_data)
 
         log(f, "{} was collected gross (net sales tax) in {}".format(gross, year))
         log(f, "{} of that was shipping".format(shipping))
