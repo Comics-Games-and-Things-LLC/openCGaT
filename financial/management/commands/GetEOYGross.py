68d92ad9a6e2d8ab738301c73199e97a590bb4c7
Noah Stephen Haskell <nstephenh@gmail.com>
2024-03-03 17:26:41 -0600

    Update EOY gross script to print over/underpaid

--- financial/management/commands/GetEOYGross.py
+++ financial/management/commands/GetEOYGross.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.core.management.base import BaseCommand
 from moneyed import Money
 from tqdm import tqdm
@@ -10,7 +12,7 @@ from partner.models import Partner
 class Command(BaseCommand):
     def handle(self, *args, **options):
         year = 2023
-        f = open("reports/eoy_gross.txt", "a")
+        f = open(f"reports/eoy_gross_{year}.txt", "a")
         partner = Partner.objects.get(name__icontains="Valhalla")
 
         valhalla_subtotal = Money("0", 'USD')
@@ -18,6 +20,7 @@ class Command(BaseCommand):
         shipping = Money(0, 'USD')
         tax = Money(0, 'USD')
         total = Money(0, 'USD')
+        total_collected = Money(0, 'USD')
         for cart in tqdm(Cart.submitted.filter(status__in=[Cart.PAID, Cart.COMPLETED], date_paid__year=year)
                                  .order_by("date_paid")):
             gross += cart.get_total_subtotal()
@@ -26,10 +29,20 @@ class Command(BaseCommand):
             tax += cart.final_tax
             total += cart.final_total
             valhalla_subtotal += cart.get_subtotal_after_cancellations()
+            if cart.total_paid:
+                if cart.total_paid.amount > (cart.final_total.amount + Decimal(.01)):
+                    log(f, f"{cart} was overpaid, we likely gave change")
+                if cart.total_paid.amount < (cart.final_total.amount - Decimal(.01)):
+                    log(f, f"{cart} was underpaid!")
+
+            # Use the minimum of final total and total paid,
+            total_collected += min(cart.final_total, cart.total_paid or Money(0, 'USD'))
+
         log(f, "{} was collected gross (net sales tax) in {}".format(gross, year))
         log(f, "{} of that was shipping".format(shipping))
         log(f, "{} was collected in tax ".format(tax))
-        log(f, "{} was collected total".format(total))
+        log(f, "{} was theoretically collected total".format(total))
+        log(f, "{} was actually collected total".format(total))
 
         log(f, "Valhalla subtotal {} (excluding cancelled lines)".format(valhalla_subtotal))
 
