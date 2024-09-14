4f0512d413877a9e392152d4b909b1088ebbc280
Noah Stephen Haskell <nstephenh@gmail.com>
2024-09-14 17:45:48 -0500

    Query ACDs website for information about new vallejo paints.

--- intake/distributors/acd.py
+++ intake/distributors/acd.py
@@ -1,8 +1,8 @@
+import pandas
 import requests
+from bs4 import BeautifulSoup
 
 from intake.models import *
-import pandas
-import xlrd
 
 dist_name = "ACD"
 npi = .47
@@ -41,3 +41,26 @@ def import_records(dist_inv_file):
                 item.dist_price = row.get('MSRP') * (1 - npi)
             item.save()
             dist_inv_file.set_availability(item, warehouse=warehouse, y_or_n=row.get('MIDDLETON'), key="YES")
+
+
+def query_for_info(upc):
+    try:
+        result = requests.get(
+            f"https://www.acdd.com/catalogsearch/advanced/result/?name=&sku=&description=&price%5Bfrom%5D=&price%5Bto%5D=&acd_upc={upc}&acd_preorder=&acd_instock=")
+        soup = BeautifulSoup(result.text, features="html5lib")
+        card_elements = soup.find_all("li", class_="item product product-item")
+        # Assume only one result since we are searching by UPC.
+        link_element = card_elements[0].find_next("a", class_="product-item-link")
+        name = link_element.get_text().strip()
+        acd_msrp = card_elements[0].find_next(
+            "div",
+            class_="msrp-value"
+        ).find_next(
+            "span",
+            class_="price"
+        ).get_text().replace("$", "")
+        return name, acd_msrp
+
+    except Exception:
+        pass
+    return None, None
--- intake/distributors/vallejo.py
+++ intake/distributors/vallejo.py
@@ -1,61 +1,62 @@
+import time
 import traceback
 
-import checkdigit.upc
-import requests
+import pandas
 from checkdigit import gs1
 
-from game_info.models import Game
+from intake.distributors import acd
+from intake.distributors.common import create_valhalla_item
 from intake.models import *
-import pandas
-import xlrd
-
-from shop.models import Product, Publisher, InventoryItem
+from shop.models import Product, Publisher
 
 
 def import_records():
     publisher, _ = Publisher.objects.get_or_create(name="Acrylicos Vallejo")
 
-    file = pandas.ExcelFile('./intake/inventories/Paints.xlsx')
-    dataframe = pandas.read_excel(file, header=0, sheet_name='Vallejo')
+    file = pandas.ExcelFile('./intake/inventories/New VMC 6pk boxes.xlsx')
+    dataframe = pandas.read_excel(file, header=0)
 
     records = dataframe.astype('string').fillna("").to_dict(orient='records')
     for row in records:
-        print(row)
         try:
-            description = "{} {}".format(row.get('Web Extended Descriptions'), row.get('Contents'))
 
-            line = row.get("Line")
-            subline = row.get("Subline")
-            number = row.get("Number")
-            if len(number) > 6:
-                number = number[:6]
-            if len(number) < 5:
-                number = number.ljust(5, '0')
+            pack_barcode = row.get('Box Barcode')
 
-            paint_name = row.get("Name")
-            size = row.get("Size (ml)")
-
-            if subline and subline.strip() != "":
-                name = "Vallejo {} {}: {}: {} {}ml".format(number, line, subline, paint_name, size)
-            else:
-                name = "Vallejo {} {}: {} {}ml".format(number, line, paint_name, size)
-
+            number = pack_barcode[-6:-1]
+            print(number)
+            display_number = f"{number[:2]}.{number[2:]}"
             barcode_start = "8429551" + number.replace('.', '')
             last_digit = gs1.calculate(barcode_start)
             barcode = barcode_start + last_digit
-            msrp = row.get('MSRP')
-            print("{} {}".format(name, barcode))
+            print(barcode)
+            # Wait 1 second before attempting to ask acd.
+            print(f"Asking ACD for the paint {display_number}")
+
+            time.sleep(1)
+            name, acd_msrp = acd.query_for_info(barcode)
+            msrp = None
+            price = None
+            if acd_msrp == "3.99":
+                msrp = "3.50"  # Standard Vallejo is 3.50 at hobbytyme, not 3.99
+                price = "3.29"  # We want to charge 3.29 at least for now.
+            print(f"{name} {barcode} ${acd_msrp}, (which we treat as ${msrp})")
+            formatted_name = f"Vallejo {display_number} {name}"
+            print(f"\t{formatted_name}")
             if barcode and barcode.strip() != '' and name and name.strip() != '':
-                product, created = Product.objects.get_or_create(
+                if Product.objects.filter(barcode=barcode).exists():
+                    print("\tProduct already exists, skipping creation...\n")
+                    continue  # Skip this product if it already exists.
+                product, created = Product.objects.create(
                     all_retail=True,
                     release_date=datetime.today(),
                     barcode=barcode,
-                    name=name,
+                    name=formatted_name,
                     publisher=publisher,
-                    msrp=msrp,
-                    description=description
+                    msrp=msrp
                 )
                 product.save()
+                create_valhalla_item(product, price)
+            print('\n')
 
         except Exception as e:
             traceback.print_exc()
