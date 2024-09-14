d2317068db54ee04cd17f566fc9c7ebf41e39bde
Noah Stephen Haskell <nstephenh@gmail.com>
2024-09-14 17:45:09 -0500

    Add price as parameter to create_valhalla_item

--- intake/distributors/common.py
+++ intake/distributors/common.py
@@ -5,20 +5,21 @@ from partner.models import Partner
 from shop.models import InventoryItem
 
 
-def create_valhalla_item(product, f=None, only_adjust_default_price=False):
+def create_valhalla_item(product, price=None, f=None, only_adjust_default_price=False):
     if f is None:
         f = open("reports/valhalla_inventory_price_adjustments.txt", "a")
 
     partner = Partner.objects.get(name__icontains="Valhalla")
 
-    price = product.get_price_from_rule(partner)
+    if price is None:
+        price = product.get_price_from_rule(partner)
+
     if price:
         item, created = InventoryItem.objects.get_or_create(partner=partner,
                                                             product=product,
                                                             defaults={
                                                                 'price': price, 'default_price': price
                                                             })
-
         if price != item.price and item.current_inventory > 0:
             # If we are only adjusting the default price, and the price change is greater than 1 cent,
             #   only adjust the default price and not the current price.
