cbd8d1aef9ae22e5a0aec212e6fa83e34a60d577
Noah Stephen Haskell <nstephenh@gmail.com>
2024-09-29 14:27:14 -0500

    Improve intake admin view so I can fix my mistakes

--- intake/admin.py
+++ intake/admin.py
@@ -2,15 +2,25 @@ from django.contrib import admin
 
 from .models import *
 
-# Register your models here.
-
 admin.site.register(Distributor)
 admin.site.register(DistributorDiscount)
 
 admin.site.register(DistItem)
 
-admin.site.register(PurchaseOrder)
-admin.site.register(POLine)
+
+class PurchaseOrderAdmin(admin.ModelAdmin):
+    search_fields = ['po_number']
+
+
+admin.site.register(PurchaseOrder, PurchaseOrderAdmin)
+
+
+class POLineAdmin(admin.ModelAdmin):
+    search_fields = ['po__po_number']
+    autocomplete_fields = ['po']
+
+
+admin.site.register(POLine, POLineAdmin)
 
 admin.site.register(DistributorWarehouse)
 admin.site.register(DistributorInventoryFile)
