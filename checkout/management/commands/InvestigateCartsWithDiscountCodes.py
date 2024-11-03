21ec124942d6960b79104d5fac3bd7e0f170874b
Noah Stephen Haskell <nstephenh@gmail.com>
2024-11-03 15:00:28 -0600

    Build report about users use if discount codes

---
+++ checkout/management/commands/InvestigateCartsWithDiscountCodes.py
@@ -0,0 +1,52 @@
+import csv
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from django.core.management.base import BaseCommand
+from tqdm import tqdm
+
+from checkout.models import Cart
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        filename = "reports/carts with discount codes.csv"
+        with open(filename, 'w', newline='') as csvfile:
+            fieldnames = ['Email', 'Username', 'First order date', "First order was discounted", 'Number of orders',
+                          'with discount codes', "without", ]
+
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for email in tqdm(Cart.submitted.values_list("email", flat=True).distinct(), unit=" Emails"):
+                carts = Cart.submitted.filter(email=email)
+                data = {"Email": email}
+                data.update(get_data_from_carts(carts))
+                writer.writerow(data)
+            for owner in tqdm(User.objects.all(), unit=" Users"):
+                carts = Cart.submitted.filter(owner=owner)
+                if not carts.exists():
+                    continue
+                data = {"Email": owner.email, "Username": owner.username}
+                data.update(get_data_from_carts(carts))
+                writer.writerow(data)
+
+        email = EmailMessage("User conversion and retention re discount codes",
+                             "Attached is the report",
+                             to=[settings.EMAIL_HOST_USER])
+        email.attach_file(filename)
+        email.send()
+        print(f"Emailed to {settings.EMAIL_HOST_USER}")
+
+
+def get_data_from_carts(carts):
+    first_cart = carts.order_by('date_submitted').first()
+    data = {
+        "First order date": first_cart.date_submitted,
+        "First order was discounted": first_cart.discount_code is not None,
+        'Number of orders': carts.count(),
+        'with discount codes': carts.filter(discount_code__isnull=False).count(),
+        'without': carts.filter(discount_code__isnull=True).count(),
+    }
+    return data
