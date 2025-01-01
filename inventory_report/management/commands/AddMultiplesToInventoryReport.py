955d2fd950b77176e7cac956d4a13bd7ca977cdf
Noah Stephen Haskell <nstephenh@gmail.com>
2025-01-01 12:11:49 -0600

    Update add multiples to inventory report to take year and location

--- inventory_report/management/commands/AddMultiplesToInventoryReport.py
+++ inventory_report/management/commands/AddMultiplesToInventoryReport.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from inventory_report.models import InventoryReport, InventoryReportLine
+from inventory_report.models import InventoryReport, InventoryReportLine, InventoryReportLocation
 from partner.models import Partner
 
 partner = Partner.objects.get(name__icontains="Valhalla")
@@ -8,18 +8,21 @@ partner = Partner.objects.get(name__icontains="Valhalla")
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("report_id", type=int)
+        parser.add_argument("year", type=int)
+        parser.add_argument("location", type=str)
         parser.add_argument("number", type=int)
         parser.add_argument("barcode", type=str)
 
     def handle(self, *args, **options):
+        year = options.pop('year')
+        location_str = options.pop('location')
         number = options.pop('number')
         barcode = options.pop('barcode')
-        report_id = options.pop('report_id')
 
-        report = InventoryReport.objects.get(id=report_id, partner=partner)
+        report = InventoryReport.objects.get(date__year=year, partner=partner)
+        location = InventoryReportLocation.objects.get(partner=partner, name__icontains=location_str)
 
         for i in range(0, number):
-            InventoryReportLine.objects.create(report=report, barcode=barcode)
+            InventoryReportLine.objects.create(report=report, barcode=barcode, location=location)
         print(f"Added {number} to {report}")
 
