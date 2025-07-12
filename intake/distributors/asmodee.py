d5b27360314e223b5b376dccb298f4de6f18e4a8
Noah Stephen Haskell <nstephenh@gmail.com>
2025-07-12 12:49:24 -0500

    Asmodee price increases 2025

--- intake/distributors/asmodee.py
+++ intake/distributors/asmodee.py
@@ -1,61 +1,34 @@
 import decimal
 import traceback
-import urllib.request
-from os.path import exists
 
 import pandas
 
 from intake.distributors.common import create_valhalla_item
 from intake.models import *
-from shop.models import Product, Publisher
+from shop.models import Product
 
 dist_name = 'Asmodee'
 
+
 def import_records():
     distributor = Distributor.objects.get_or_create(dist_name=dist_name)[0]
+    f = open("reports/valhalla_inventory_price_adjustments.txt", "a")
+    partner = Partner.objects.get(name__icontains="Valhalla")
 
-
-    timestamp = datetime.today().date().strftime("%Y%m%d")
-    filename = "./intake/inventories/Asmodee-{}.csv".format(timestamp)
-    if not exists(filename):
-        print("Downloading today's Asmodee inventory")
-        try:
-            opener = urllib.request.build_opener()
-            opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-            urllib.request.install_opener(opener)
-            urllib.request.urlretrieve("https://www.asmodeena.com/active-catalog-csv", filename)
-        except urllib.error.HTTPError as e:
-            print(e)
-            print(e.fp.read())
-            exit()
-    else:
-        print("Using previously downloaded Asmodee inventory")
+    filename = "./intake/inventories/SRP and MAP Increases - 7.1.25.xlsx - Sheet1.csv"
     dataframe = pandas.read_csv(filename, header=0, encoding='latin1')
-
     records = dataframe.astype('string').fillna("").to_dict(orient='records')
     for row in records:
         # print(row)
         try:
-            publisher, _ = Publisher.objects.get_or_create(name=row.get('Studio'))
 
-            rawdate = row.get('Release Date')
-
-            date = datetime(2022, 1, 1)  # Default date to today.
-            try:
-                if rawdate != '':
-                    date = datetime.strptime(rawdate, '%m/%d/%Y').date()
-            except ValueError:
-                pass
-
-            msrpstring = str(row.get('MSRP', row.get(" MSRP "))).replace("$", "").strip()
-            mapstring = str(row.get('MAP', row.get(" MAP "))).replace("$", "").replace("-", "").strip()
+            msrpstring = str(row.get('New SRP')).replace("$", "").strip()
+            mapstring = str(row.get('New MAP')).replace("$", "").replace("-", "").strip()
 
             msrp = Money(msrpstring, 'USD')
-            maprice = None
+            maprice = Money(0, 'USD')
             try:
                 maprice = Money(mapstring, 'USD')
-                if maprice.amount == 0:
-                    maprice = 0
             except decimal.InvalidOperation:
                 pass
 
@@ -79,14 +52,19 @@ def import_records():
                 if products.count() == 1:
                     product = products.first()
                     product.all_retail = True
+                    old_msrp = product.msrp
                     product.msrp = msrp
-                    product.map = maprice
-                    product.publisher = publisher
-                    if rawdate:
-                        product.release_date = date
+                    if maprice.amount == 0:
+                        product.map = None
+                    else:
+                        product.map = maprice
                     product.save()
-                    # For now lets not adjust all the prices on Asmodee items, since the pricing rules are all up in the air.
-                    # create_valhalla_item(product)
+                    if maprice.amount > 1:
+                        new_price = maprice
+                    else:
+                        new_price = product.get_price_from_rule(partner)
+                    create_valhalla_item(product, f=f, price=new_price)
+
 
         except Exception as e:
             traceback.print_exc()
