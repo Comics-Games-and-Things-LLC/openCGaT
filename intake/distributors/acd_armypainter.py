cbd4c1171c58e565b20e5b21941a441d2d322a71
Noah Stephen Haskell <nstephenh@gmail.com>
2025-08-08 18:21:22 -0500

    Import Army Painter from ACD

---
+++ intake/distributors/acd_armypainter.py
@@ -0,0 +1,109 @@
+import decimal
+import json
+import traceback
+from decimal import ROUND_DOWN
+
+import pandas
+import requests
+from bs4 import BeautifulSoup
+
+from intake.models import *
+
+dist_name = "ACD_ARMY_PAINTER"
+npi = .47
+
+
+
+
+
+
+def query_for_info(sku, get_full=False, debug=False):
+    if sku is None or "":
+        return {}
+    try:
+        result = requests.get(
+            f"https://www.acdd.com/search-advanced/results?sku={sku}&manufacturers=218&sort=sku_a_to_z")
+        soup = BeautifulSoup(result.text, features="html5lib")
+        script_elements = soup.find_all("script")
+        # Assume only the first result matters since we are searching by UPC.
+        page_data = ""
+        prefix = 'self.__next_f.push([1,"'
+        for script in script_elements:
+            if not script.string:
+                continue
+            if prefix not in script.string:
+                continue
+            page_data += script.string[len(prefix):-len("\\n\")]")]
+
+        readable_data = bytes(page_data, "utf-8").decode("unicode_escape")
+        last_line = readable_data.splitlines()[-1]
+        five_data = "[" + last_line.split("5:[")[1]
+        page_json_data = json.loads(five_data)
+        results = []
+        search_key(page_json_data, "products", results)
+        item_details = results[0][0]
+        if debug:
+            print(json.dumps(item_details, sort_keys=True, indent=4))
+
+        msrp = None
+        msrp_object = item_details["prices"].get("retailPrice")
+        if msrp_object:
+            msrp = decimal.Decimal(msrp_object["value"]).quantize(Decimal('.01'), rounding=ROUND_DOWN)
+        # Don't trust any of the other prices to be the price.
+
+        for field in item_details["customFields"]:
+            item_details[field["name"]] = field["value"]
+
+        default_image = item_details["defaultImage"]
+        image_url = None
+        if default_image:
+            image_url = default_image["url"]
+
+        return {
+            "Name": fix_text(item_details["name"]),
+            "MSRP": msrp,
+            "Barcode": item_details["upc_code"],
+            "SKU": item_details["sku"],
+            "Description": fix_text(item_details["description"]),
+            "Picture Source": image_url,
+            "Release Date": item_details.get("release_date"),
+            "Publisher": item_details["manufacturer_code"],  # TODO: translate into a category
+        }
+    except Exception as e:
+        print(f"error for {sku}: {e}")
+        if debug:
+            traceback.print_exc()
+
+    return {}
+
+
+def fix_text(text):  # Fix bad encoding and invalid HTML
+    fixed_text = text.encode('latin-1').decode('utf-8')
+    if "<" in fixed_text:
+        return str(BeautifulSoup(fixed_text, "html5lib"))
+    return fixed_text
+
+
+def get_from_table(soup, row_heading):
+    value = None
+    release_date_elements = soup.find_all('td', class_="col data", attrs={"data-th": row_heading})
+    if release_date_elements:
+        value = release_date_elements[0].get_text()
+    return value
+
+
+def search_key(data, target_key, results=None):
+    if results is None:
+        results = []
+
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key == target_key:
+                results.append(value)
+            search_key(value, target_key, results)  # Recursive call
+
+    elif isinstance(data, list):
+        for item in data:
+            search_key(item, target_key, results)  # Recursive call
+
+    return results
---
+++ intake/management/commands/ImportArmyPainter.py
@@ -0,0 +1,47 @@
+import time
+
+from django.core.management.base import BaseCommand
+
+from intake.distributors.acd_armypainter import query_for_info
+from intake.models import *
+from shop.models import Publisher, InventoryItem
+
+
+class Command(BaseCommand):
+    help = "Download Army Painter Fanatic from ACD"
+
+    def handle(self, *args, **options):
+        category = Category.objects.get(id=2337)
+        publisher = Publisher.objects.get(name="Army Painter")
+        partner = Partner.objects.get(name__icontains="Valhalla")
+
+        for i in range(1, 217):
+            sku = f"AMYWP3{str(i).rjust(3, "0")}"
+            print(sku)
+            if Product.objects.filter(publisher_sku=sku).exists():
+                product = Product.objects.get(publisher_sku=sku)
+            else:
+                time.sleep(1)
+                info = query_for_info(sku, debug=False)
+                product = Product.create_from_dist_info(info)
+            product.publisher = publisher
+            product.categories.add(category)
+            product.page_is_draft = False
+            product.visible_on_release = True
+            product.listed_on_release = True
+            product.purchasable_on_release = True
+            product.save()
+            price = product.get_price_from_rule(partner)
+
+            item, created = InventoryItem.objects.get_or_create(partner=partner,
+                                                                product=product,
+                                                                defaults={
+                                                                    'price': price, 'default_price': price
+                                                                })
+            if not created:
+                item.price = price
+                item.default_price = price
+            item.allow_backorders = False
+            item.enable_restock_alert = True
+            item.low_inventory_alert_threshold = 1
+            item.save()
