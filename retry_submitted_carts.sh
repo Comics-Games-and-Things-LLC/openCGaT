11ff113551f3d72c05a731a76eb8fd784d566d34
Noah Stephen Haskell <nstephenh@gmail.com>
2025-09-21 17:05:33 -0500

    Retry cart script should only run at 4am

--- retry_submitted_carts.sh
+++ retry_submitted_carts.sh
@@ -2,6 +2,7 @@ while true
 do
   ./manage.py RetrySubmittedCarts
   date +"%H:%M:%S"
-  echo "Waiting 2 hours"
-  sleep 2h
+  echo "Waiting until $(printf "tomorrow 4:00"  | date -f -)"
+  sleep $(( $( printf '%s\nnow\n' "tomorrow 4:00" | date -f - +%s- ) 0 ))
+
 done
