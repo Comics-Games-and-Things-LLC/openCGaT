5353aaf8ea524f615d3ef4fc315ce1ec95572512
Noah Stephen Haskell <nstephenh@gmail.com>
2026-03-08 17:09:28 -0500

    Run CheckUploadedInventories repeatedly

--- intake/management/commands/CheckUploadedInventories.py
+++ intake/management/commands/CheckUploadedInventories.py
@@ -2,17 +2,19 @@ import time
 
 from django.core.management.base import BaseCommand
 
-from intake.distributors import games_workshop
 from intake.models import DistributorInventoryFile
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        while True:
+            # Run this loop every 30 seconds.
+            self.recurring_logic()
+            time.sleep(30)
+
+    @staticmethod
+    def recurring_logic():
         for inv in DistributorInventoryFile.objects.filter(processed=False):
             inv.run_import()
             inv.processed = True
             inv.save()
-        time.sleep(30)
-
-
-
